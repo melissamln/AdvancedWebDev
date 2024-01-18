726f89e9894efe5dccc99af097759a52bbee0446
melissamln <melissamelin06@gmail.com>
2024-01-18 14:51:06 +0100

    added stores.json

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const stores = require('./stores.json'); 
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.json('Hello, World!');
 });
 
 app.listen(port, () => {
