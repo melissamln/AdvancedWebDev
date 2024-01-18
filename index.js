8bfaef605a92af676b10d7aee8ae424fa00954bb
melissamln <melissamelin06@gmail.com>
2024-01-18 15:21:15 +0100

    created params

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const port = 3000;
 
 const stores = require('./stores.json'); 
 
-app.get('/', (req, res) => {
-  res.json(stores);
+app.get('/stores/:storename', (req, res) => {
+  const{storename} = req.params;
+  console.log(storename);
+  const storeIndex = stores.findIndex ((store) => store.name === storename); // for loop that returns the index of that line
+  console.log(`Store index is ${storeIndex}`);
+  if (storeIndex === -1){
+    res.send('Not found');
+  } else {
+    res.json(stores[stores]);
+  }
+});
+
+app.get('/stores/:district', (req, res) => {
+  const {district} = req.params;
+  const storesInDistrict = stores.filter((store) => store.district === district);
+  res.json(storesInDistrict);
 });
 
 app.listen(port, () => {
