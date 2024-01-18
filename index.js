1756464de593ee40f6fc2a977bff45e8bda8bf5f
DinaMostafa00 <modi21tf@student.ju.se>
2024-01-18 16:10:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,29 +4,45 @@ const port = 3000;
 
 const stores = require("./stores.json");
 
+// app.get("/", (req, res) => {
+//   const { storename } = req.query;
+// });
+
+//store is the equlivent to a "row"
+// ? means query params
+
 app.get("/", (req, res) => {
   const { storename } = req.query;
-});
-
-app.get("/stores/:storename", (req, res) => {
-  const { storename } = req.params;
   console.log(storename);
   const storeIndex = stores.findIndex((store) => store.name === storename); // for loop that returns the index of that line
   console.log(`Store index is ${storeIndex}`);
   if (storeIndex === -1) {
     res.send("Not found");
   } else {
-    res.json(stores[stores]);
+    res.json(stores[storeIndex]);
   }
 });
 
-app.get("/stores/:district", (req, res) => {
-  const { district } = req.params;
-  const storesInDistrict = stores.filter(
-    (store) => store.district === district
-  );
-  res.json(storesInDistrict);
+app.delete("/", (req, res) => {
+  const { storename } = req.query;
+  const storeIndex = stores.findIndex((row) => row.name === storename); // for loop that returns the index of that line
+  if (storeIndex === -1) {
+    stores.splice(storeIndex, 1);
+    res.send("store deleted");
+  } else {
+    res.send("store not found");
+  }
 });
+
+// the slash in the dynmic link thingy
+
+// app.get("/stores/:district", (req, res) => {
+//   const { district } = req.params;
+//   const storesInDistrict = stores.filter(
+//     (store) => store.district === district
+//   );
+//   res.json(storesInDistrict);
+// });
 
 //adding a store but not the the json
 //  coma in express means move to the next thing
