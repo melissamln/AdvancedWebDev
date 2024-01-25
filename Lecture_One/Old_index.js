1f70480291195b890f112e13123f7e77965d2f08
DinaMostafa00 <modi21tf@student.ju.se>
2024-01-25 14:42:55 +0100

    week2

--- Lecture_One/Old_index.js
+++ Lecture_One/Old_index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const stores = require("./stores.json");
+const stores = require("../stores.json");
 
 // app.get("/", (req, res) => {
 //   const { storename } = req.query;
